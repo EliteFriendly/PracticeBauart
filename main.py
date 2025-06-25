a0e6beae4c3d65369c51e82096dede3b53df316d
EliteFriendly <nikitabazin2004@mail.ru>
2025-06-25 12:19:08 +0300

    Create a docker website with file upload

--- main.py
+++ main.py
@@ -1,7 +1,19 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, UploadFile, File
 
 app = FastAPI()
 
 @app.get("/")
 def root():
     return {"message": "Hello World"}
+
+
+
+@app.post("/upload")
+def upload(file: UploadFile = File(...)):
+    try:
+        file_path = f"test-files/{file.filename}"
+        with open(file_path, "wb") as f:
+            f.write(file.file.read())
+        return {"message": "File saved successfully"}
+    except Exception as e:
+        return {"message": e.args}
--- myenv/bin/activate.fish
+++ myenv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV /home/bauart/nikita/myenv
+set -gx VIRTUAL_ENV /home/bauart/PracticeBauart/myenv
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/"bin $PATH
