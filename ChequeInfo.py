c97a311c87f79566d9e58f065bd9a070b20bb354
EliteFriendly <nikitabazin2004@mail.ru>
2025-07-01 07:51:13 +0300

    Added request error processing

--- ChequeInfo.py
+++ ChequeInfo.py
@@ -4,12 +4,12 @@ import os
 
 
 class ChequeInfo:
-    __goodIMGExtension = [".jpg",".pdf",".png",".jpeg"]
+    __goodIMGExtension = ("bmp", "gif", "jpeg", "png", "tiff", "pdf",)
     __token = os.getenv("TOKEN") # token from website: https://proverkacheka.com 
     __websiteAPI = os.getenv("API") #API to get info from cheques from website
 
 
-    __columnsName = ["name","productType","quantity","price","sum","data"]
+    __columnsName = ("name","productType","quantity","price","sum","data",)
     __dictProducts = {
         "name":[],
         "productType":[],
@@ -33,7 +33,7 @@ class ChequeInfo:
                  typeIMG = True
        
         if not typeIMG:
-            raise NameError("Wrong file type, can be only: .jpg, .pdf, .png, .jpeg")
+            raise NameError("Wrong file type, can be only: bmp, gif, jpeg, png, tiff, pdf")
 
         files = {"qrfile": open(fileName, "rb")}# For set image to API
 
@@ -41,7 +41,19 @@ class ChequeInfo:
         "token": self.__token,
          }
         r = requests.post(url=self.__websiteAPI, data=data, files=files)
-
+        
+        #If was error from request
+        if not (r.json()["code"] == 1):
+            codeError = r.json()["code"]
+            with open('data_failed.json', 'w') as outfile:
+                json.dump(r.json(), outfile)
+            raise NameError (f"Error cheque read, error:{codeError}") 
+            '''
+            You can watch code error in: https://view.officeapps.live.com/op/view.aspx?src=https%3A%2F%2Fproverkacheka.com%2Ffiles%2Fhelp%2Fdocumentation_api.docx&wdOrigin=BROWSELINK
+            But also code-3 can be code-0 or code-2
+            '''
+        
+        
         #Get info from dict, for future update __dictProducts
         namesArr = self.__dictProducts["name"]
         productTypesArr = self.__dictProducts["productType"]
@@ -69,6 +81,6 @@ class ChequeInfo:
 
         
     
-    def getDictProducts(self):
+    def getListProducts(self):
         return self.__dictProducts
             
