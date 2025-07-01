92d3a8d40db9092bdfa822049cd4ac71dd23a8a7
EliteFriendly <nikitabazin2004@mail.ru>
2025-07-01 07:24:01 +0300

    Rewriting the way goods are stored in "chequeInfo" to dict

--- ChequeInfo.py
+++ ChequeInfo.py
@@ -1,25 +1,32 @@
-import requests
-import json
-import pandas as pd
+import requests, json
 import os
 
 
 
 class ChequeInfo:
-    __goodIMGExtension = (".jpg",".pdf",".png",".jpeg",)
+    __goodIMGExtension = [".jpg",".pdf",".png",".jpeg"]
     __token = os.getenv("TOKEN") # token from website: https://proverkacheka.com 
     __websiteAPI = os.getenv("API") #API to get info from cheques from website
 
-    __columnsName = ("name","productType","quantity","price","sum","data",)
-    __listProducts = pd.DataFrame(columns=__columnsName) 
+
+    __columnsName = ["name","productType","quantity","price","sum","data"]
+    __dictProducts = {
+        "name":[],
+        "productType":[],
+        "quantity":[],
+        "price":[],
+        "sum":[],
+        "data":[],
+    } 
     '''
     price in next format: 300.50, where 300 its rubles, and 50 its pennies.
-    data in next format: YYYY-MM-DD HH:MM:SS
+    data in next format: YYYY-MM-DDTHH:MM:SS, where T - special symbol from API
     productType: can be type from productType.txt
     '''
 
 
     def setQRImage(self,fileName = ""):
+               
         typeIMG = False
         for i in range(len(self.__goodIMGExtension)):# Checking the file type
             if not self.__goodIMGExtension[i] in fileName:
@@ -34,20 +41,34 @@ class ChequeInfo:
         "token": self.__token,
          }
         r = requests.post(url=self.__websiteAPI, data=data, files=files)
+
+        #Get info from dict, for future update __dictProducts
+        namesArr = self.__dictProducts["name"]
+        productTypesArr = self.__dictProducts["productType"]
+        quantityArr = self.__dictProducts["quantity"]
+        priceArr = self.__dictProducts["price"]
+        sumArr = self.__dictProducts["sum"]
+        datasArr = self.__dictProducts["data"]
         
         for i in range(len(r.json()["data"]["json"]["items"])):# take only items/products from .json and set its into a DataFrame
-            tmp = []
-            tmp.append(r.json()["data"]["json"]["items"][i]["name"])
-            tmp.append("None")
-            tmp.append(r.json()["data"]["json"]["items"][i]["quantity"])
-            tmp.append(r.json()["data"]["json"]["items"][i]["price"]/100.0)
-            tmp.append(tmp[2]*tmp[3])
-            tmp.append(r.json()["data"]["json"]["dateTime"])
-             
-            self.__listProducts.loc[-1] = tmp
-            self.__listProducts.index = self.__listProducts.index + 1 
+            namesArr.append(r.json()["data"]["json"]["items"][i]["name"])
+            productTypesArr.append("None")
+            quantityArr.append(r.json()["data"]["json"]["items"][i]["quantity"])
+            priceArr.append(r.json()["data"]["json"]["items"][i]["price"]/100.0)
+            sumArr.append(quantityArr[len(quantityArr)-1]*priceArr[len(priceArr)-1])
+            datasArr.append(r.json()["data"]["json"]["dateTime"])
+        
+        #Update __dictProducts
+        self.__dictProducts["name"] = namesArr
+        self.__dictProducts["productType"] = productTypesArr
+        self.__dictProducts["quantity"] = quantityArr
+        self.__dictProducts["price"] = priceArr
+        self.__dictProducts["sum"] = sumArr
+        self.__dictProducts["data"] = datasArr
+        
+
         
     
-    def getListProducts(self):
-        return self.__listProducts.sort_index()
+    def getDictProducts(self):
+        return self.__dictProducts
             
--- uploadIMGRoute.py
+++ uploadIMGRoute.py
@@ -7,12 +7,12 @@ QRreader = ChequeInfo
 @uploadIMG.post("/uploadIMG")
 def  sendToChequeInfo(file: UploadFile = File(...)):
     file_path = f"test-files/{file.filename}"
-    QRreader.setQRImage(QRreader,fileName=file_path)
     with open(file_path, "wb") as tmpF:
         tmpF.write(file.file.read())
+    QRreader.setQRImage(QRreader,fileName=file_path)
     return {"message": "File saved successfully"}
     
 @uploadIMG.get("/showProductsList")
 def getProductList():
-    return QRreader.getListProducts(QRreader)
+    return QRreader.getDictProducts(QRreader)
 
